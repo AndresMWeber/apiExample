359e2468a51359185130fa968ddd455c28202463
Andres Weber <andresmweber@gmail.com>
2019-09-16 10:43:32 -0400

    10:45 commit, updated

--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const Pokemon = require('../models/monster');
 
 
 /* GET home page */
 router.get('/', (req, res, next) => {
     axios.get('http://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000')
         .then(allPokes => {
-            console.log('all the pokes =====>>>>>>>>', allPokes.data.results)
-            res.render('apiViews/apiHome', allPokes.data.results);
+            res.render('apiViews/apiHome', { allThePokemon: allPokes.data.results });
         })
         .catch(err => next(err))
 });
 
 router.get('/poke/:pokeId', (req, res, next) => {
-    //                |
-    //                ------------------------------------------
-    //                                                          |
-    axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.pokeId}`)
-        .then(responseFromAPI => {
-            console.log("><>><<<><><><><><> ", responseFromAPI.data);
+    Pokemon.findOne({ pokeId: Number(req.params.pokeId) + 1 })
+        .then(pokeFromDb => {
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${Number(req.params.pokeId) + 1}`)
+                .then(responseFromAPI => {
+                    console.log("><>><<<><><><><><> ", responseFromAPI.data);
+                    data = {
+                        pokes: responseFromAPI.data,
+                        isSaur: responseFromAPI.data.name.includes('saur')
+                    };
 
-            data = {
-                pokes: responseFromAPI.data,
-                isSaur: true
-            };
-
-            if (!responseFromAPI.data.name.includes('saur')) data.isSaur = false
-
-            res.render('apiViews/details', data);
+                    res.render('apiViews/apiDetails', data);
+                }).catch(err => next(err));
         }).catch(err => next(err));
+
 });
 
 module.exports = router;
