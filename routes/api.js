048caecf64cce901719c184eac33fc3f2f2f6e3a
Andres Weber <andresmweber@gmail.com>
2019-09-16 09:37:25 -0400

    Starting heroku deploy.

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,25 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const axios = require('axios');
 
 
 router.get('/poke/:pokeId', (req, res, next) => {
-  //                |
-  //                ------------------------------------------
-  //                                                          |
-  axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.pokeId}`)
-  .then(responseFromAPI => {
-    console.log("><>><<<><><><><><> ", responseFromAPI.data);
+    //                |
+    //                ------------------------------------------
+    //                                                          |
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${req.params.pokeId}`)
+        .then(responseFromAPI => {
+            console.log("><>><<<><><><><><> ", responseFromAPI.data);
 
-    data = {
-      pokes: responseFromAPI.data,
-      loadedInfo: true
-    };
+            data = {
+                pokes: responseFromAPI.data,
+                isSaur: true
+            };
 
-    if(!responseFromAPI.data.name.includes('saur')) {
-      data.loadedInfo = false;
-    }
+            if (!responseFromAPI.data.name.includes('saur')) data.isSaur = false
 
-    res.render('apiViews/apiHome', data);
-  }).catch(err => next(err));
+            res.render('apiViews/details', data);
+        }).catch(err => next(err));
 });
 
 module.exports = router;
