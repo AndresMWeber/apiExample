14ea3b940ccd07e4fb3c65463d8e8c085f6995a0
Andres Weber <andresmweber@gmail.com>
2019-09-16 10:04:56 -0400

    10AM commit, current.

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const axios = require('axios');
 
 
+/* GET home page */
+router.get('/', (req, res, next) => {
+    axios.get('http://pokeapi.co/api/v2/pokemon/?offset=0&limit=1000')
+        .then(allPokes => {
+            console.log('all the pokes =====>>>>>>>>', allPokes.data.results)
+            res.render('apiViews/apiHome', allPokes.data.results);
+        })
+        .catch(err => next(err))
+});
+
 router.get('/poke/:pokeId', (req, res, next) => {
     //                |
     //                ------------------------------------------
